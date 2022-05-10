aa7aa4031207dece12cb4a298002ec82ae2b71da
Rickh <hpx210650@gmail.com>
2022-05-10 23:10:55 +0800

    add simple test

---
+++ tests/App.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react';
+import App from '../src/App';
+
+describe('App.jsx', () => {
+  beforeAll(() => {
+    render(<App />)
+  });
+
+  it('渲染 App.jsx', () => {
+    const message = 'App';
+
+    expect(screen.getByText(message)).toBeInTheDocument()
+  })
+
+  afterAll(cleanup);
+});
