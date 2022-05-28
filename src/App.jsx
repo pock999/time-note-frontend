c119861f82d8eadf5e95d5b1b7e45dd7af48106e
Rickh <hpx210650@gmail.com>
2022-05-28 11:43:26 +0800

    ✨ feat: 語言:繁中

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { zhTW } from '@mui/material/locale';
+
 import PathRouter from './router';
 import store from './store';
 
+const theme = createTheme(
+  {},
+  zhTW,
+);
+
 const App = () => {
   return (
-    <Provider store={store}>
-      <PathRouter />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PathRouter />
+      </Provider>
+    </ThemeProvider>
   );
 };
 
