d800d6ef80c21d0365ccc0507ce5bad19f114ad3
Rickh <hpx210650@gmail.com>
2022-06-12 13:41:15 +0800

    ✨ feat: Profile UI

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -16,6 +16,17 @@ import {
   Button,
   Typography,
   Divider,
+  InputLabel,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Select,
+  MenuItem,
+  FormLabel,
+  FormGroup,
+  Switch,
+  FormControlLabel,
 } from '@mui/material';
 
 import { BaseLayout } from '../layouts';
@@ -29,6 +40,8 @@ export default function Profile() {
 
   const userProfile = useSelector((state) => state.auth.user);
 
+  const [isResetPwd, setIsResetPwd] = React.useState(false);
+
   React.useEffect(() => {
     dispatch(getProfileAction());
   }, []);
@@ -38,7 +51,66 @@ export default function Profile() {
   return (
     <BaseLayout>
       <Container sx={{ paddingTop: '2em', paddingBottom: '2em', marginBottom: '2em' }}>
-        Profile
+        <Typography variant="h5" sx={{ marginBottom: '1em' }}>行事曆</Typography>
+        {
+          userProfile && (
+            <>
+              <TextField
+                id="email"
+                label="Email"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={userProfile.email}
+                margin="dense"
+                disabled
+              />
+              <TextField
+                id="name"
+                label="姓名"
+                type="text"
+                fullWidth
+                variant="outlined"
+                value={userProfile.name}
+                margin="dense"
+              />
+              {
+                isResetPwd && (
+                  <>
+                    <TextField
+                      id="password"
+                      label="密碼"
+                      type="password"
+                      fullWidth
+                      variant="outlined"
+                      margin="dense"
+                    />
+                    <TextField
+                      id="repeat-password"
+                      label="密碼確認"
+                      type="password"
+                      fullWidth
+                      variant="outlined"
+                      margin="dense"
+                    />
+                  </>
+                )
+              }
+              <FormControl component="fieldset" variant="standard">
+                <FormLabel component="legend">是否更改密碼</FormLabel>
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Switch checked={isResetPwd} onChange={() => setIsResetPwd((pre) => !pre)} name="gilad" />
+                    }
+                  />
+                </FormGroup>
+              </FormControl>
+
+            </>
+          )
+        }
+
       </Container>
     </BaseLayout>
   );
