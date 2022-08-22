2b657dd0fd0363917257c42d9e93a764e2923ddc
Rickh <hpx210650@gmail.com>
2022-08-23 02:41:30 +0800

    Updates

---
+++ src_views_Page404_jsx.bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktime_note_frontend"] = self["webpackChunktime_note_frontend"] || []).push([["src_views_Page404_jsx"],{
+
+/***/ "./src/views/Page404.jsx":
+/*!*******************************!*\
+  !*** ./src/views/Page404.jsx ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Page404)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n // import Container from '@mui/material/Container';\n\nfunction Page404() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, \"Page404\");\n}\n\n//# sourceURL=webpack://time-note-frontend/./src/views/Page404.jsx?");
+
+/***/ })
+
+}]);
